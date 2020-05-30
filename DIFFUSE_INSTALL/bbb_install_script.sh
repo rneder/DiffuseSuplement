2d3a250747b0771093187fcca1ff9209e0b5d07b
rneder <reinhard.neder@fau.de>
2020-05-30 23:36:22 +0200

    Corrected source for DIFFUSE_INSTALL to DiffuseSuplement

--- DIFFUSE_INSTALL/bbb_install_script.sh
+++ DIFFUSE_INSTALL/bbb_install_script.sh
@@ -13,8 +13,10 @@ export DISCUS_LOCAL=1
 export DISCUS_INST_TYPE=$DISCUS_WEB
 export DISCUS_VERSION=$(curl --silent "https://github.com/tproffen/DiffuseCode/releases/latest" | grep -Poe 'v.[0-9]*.[0-9]*.[0-9]*')
 export DISCUS_CODE_URL='https://github.com/tproffen/DiffuseCode/archive/'${DISCUS_VERSION}'.tar.gz'
-export DISCUS_INSTALL_URL='https://github.com/tproffen/DiffuseCode/releases/download/'${DISCUS_VERSION}'/DIFFUSE_INSTALL.tar.gz'
-export PGPLOT_CODE_URL='https://github.com/tproffen/DiffuseCode/releases/download/'${DISCUS_VERSION}'/DIFFUSE_CODE_pgplot.tar.gz'
+#
+export DISCUS_SUPPLEMENT=$(curl --silent "https://github.com/rneder/DiffuseSuplement/releases/latest" | grep -Poe 'v.[0-9]*.[0-9]*.[0-9]*')
+export DISCUS_INSTALL_URL='https://github.com/rneder/DiffuseSuplement/releases/download/'${DISCUS_SUPPLEMENT}'/DIFFUSE_INSTALL.tar.gz'
+export PGPLOT_CODE_URL='https://github.com/rneder/DiffuseSuplement/releases/download/'${DISCUS_SUPPLEMENT}'/DIFFUSE_CODE_pgplot.tar.gz'
 #
 echo Starting to download the DISCUS_SUITE installation parts
 echo This may take a moment, please be patient
