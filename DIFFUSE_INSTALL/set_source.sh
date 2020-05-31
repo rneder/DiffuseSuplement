93215c43e484d43f7b30603a28e6e58df1cbc38f
rneder <reinhard.neder@fau.de>
2020-05-31 17:56:27 +0200

    Moved section from install_discus.sh

--- DIFFUSE_INSTALL/set_source.sh
+++ DIFFUSE_INSTALL/set_source.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 #
+#
+#  If called without parameters, install DISCUS from GITHUB
+#
+if [ "$#" -ne 1 ]; then
+  echo 
+  echo " Installation of DISCUS_SUITE from code at github"
+  echo " Download may take a moment, please be patient "
+  echo
+  curl -o DIFFUSE_CODE.tar.gz -fSL ${DISCUS_CODE_URL}
+  export DISCUS_TAR_SOURCE=DIFFUSE_CODE.tar.gz
+  echo
+  echo " Download of ${DISCUS_TAR_SOURCE} Version ${DISCUS_VERSION} is complete "
+  echo
+else
+  export DISCUS_TAR_SOURCE=$1
+  echo
+  echo " Installation from local archive " ${DISCUS_TAR_SOURCE}
+  echo
+fi
+#
 mkdir -p $DISCUS_DEVELOP
 #
-#if [ "$#" -eq 1 ]; then
-  cp $DISCUS_TAR_SOURCE $DISCUS_DEVELOP
-  cd $DISCUS_DEVELOP
-  export DISCUS_CODE_DIR=$(tar -ztf $DISCUS_TAR_SOURCE | head -1 | cut -d "/" -f 1)
-  tar -zxf $DISCUS_TAR_SOURCE
-  if ! [ $DISCUS_CODE_DIR ==  'DiffuseCode' ]; then
-     rm -rf $DISCUS_DEVELOP/DiffuseCode
-     mv $DISCUS_DEVELOP/$DISCUS_CODE_DIR $DISCUS_DEVELOP/DiffuseCode
-  fi
-#fi
+cp $DISCUS_TAR_SOURCE $DISCUS_DEVELOP
+cd $DISCUS_DEVELOP
+export DISCUS_CODE_DIR=$(tar -ztf $DISCUS_TAR_SOURCE | head -1 | cut -d "/" -f 1)
+tar -zxf $DISCUS_TAR_SOURCE
+if ! [ $DISCUS_CODE_DIR ==  'DiffuseCode' ]; then
+   rm -rf $DISCUS_DEVELOP/DiffuseCode
+   mv $DISCUS_DEVELOP/$DISCUS_CODE_DIR $DISCUS_DEVELOP/DiffuseCode
+fi
 #
 rm -rf   $DISCUS_DEVELOP/DiffuseBuild
 mkdir -p $DISCUS_DEVELOP/DiffuseBuild
