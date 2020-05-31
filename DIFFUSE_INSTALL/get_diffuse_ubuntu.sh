141d6a57a384c4780f445981e559ca15c3d2112e
rneder <reinhard.neder@fau.de>
2020-05-31 17:55:41 +0200

    Added better echo

--- DIFFUSE_INSTALL/get_diffuse_ubuntu.sh
+++ DIFFUSE_INSTALL/get_diffuse_ubuntu.sh
@@ -4,10 +4,15 @@ if [ ! -e DIFFUSE_${OPERATING_NAME}_${OPERATING_VERSION}.tar.gz ]; then
 #
 #  File does not exist, obtain
 #
+  export DISCUS_SUP_DOWN='https://github.com/rneder/DiffuseSuplement/releases/download/'
   export DISCUS_SUPPLEMENT=$(curl --silent "https://github.com/rneder/DiffuseSuplement/releases/latest" | grep -Poe 'v.[0-9]*.[0-9]*.[0-9]*')
-  export DISCUS_UBUNTU_URL='https://github.com/rneder/DiffuseSuplement/releases/download/'${DISCUS_SUPPLEMENT}'/DIFFUSE_${OPERATING_NAME}_'${OPERATING_VERSION}'.tar.gz'
+  export DISCUS_UBUNTU_URL=${DISCUS_SUP_DOWN}${DISCUS_SUPPLEMENT}'/DIFFUSE_'${OPERATING_NAME}'_'${OPERATING_VERSION}'.tar.gz'
   if curl --output /dev/null --silent --head --fail "${DISCUS_UBUNTU_URL}"; then
+    echo
+    echo "Downloading ${DISCUS_UBUNTU_URL}"
+    echo
     curl -o DIFFUSE_${OPERATING_NAME}_${OPERATING_VERSION}.tar.gz -SL ${DISCUS_UBUNTU_URL}
+    echo
   else
     echo ${DISCUS_UBUNTU_URL} does not exist exists
   fi
