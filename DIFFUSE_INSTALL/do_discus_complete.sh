ed1d1331378c1cd128bee6d550c103af49be5f6b
rneder <reinhard.neder@fau.de>
2020-06-04 22:48:21 +0200

    Adapted to different operating systems

--- DIFFUSE_INSTALL/do_discus_complete.sh
+++ DIFFUSE_INSTALL/do_discus_complete.sh
@@ -1,20 +1,45 @@
-    export DIFFEV_MPI_FLAG=OFF
-    source   ./compile_discus.sh clean
-    if [[ $DISCUS_INSTALL == $DISCUS_LOCAL ]]; then
-      cp /usr/local/bin/discus_suite /usr/local/bin/discus_suite_noparallel
-    else
-      sudo cp /usr/local/bin/discus_suite /usr/local/bin/discus_suite_noparallel
-    fi
-    export DIFFEV_MPI_FLAG=ON
-    source   ./compile_discus.sh noclean
+#!/bin/bash
 #
-    cd $DISCUS_INST_DIR
-    if [[ $DISCUS_INSTALL == $DISCUS_LOCAL ]]; then
-      cp SHELLS/discus_suite_ubuntu.sh     /usr/local/bin
-      cp SHELLS/discus_suite_run_ubuntu.sh /usr/local/bin
-      cp SHELLS/terminal_wrapper.sh        /usr/local/bin
-    else
-      sudo cp SHELLS/discus_suite_ubuntu.sh     /usr/local/bin
-      sudo cp SHELLS/discus_suite_run_ubuntu.sh /usr/local/bin
-      sudo cp SHELLS/terminal_wrapper.sh        /usr/local/bin
-    fi
+#  Complete compilation of DISCUS
+#
+if [[ "$OPERATING" == "DISCUS_WSL_LINUX" ]]; then          # WINDOWS WSL 
+#
+#  Both nonparallel and parallel versions are needed
+#
+  export DIFFEV_MPI_FLAG=OFF
+  source   ./compile_discus.sh clean
+  if [[ $DISCUS_INSTALL == $DISCUS_LOCAL ]]; then
+    cp /usr/local/bin/discus_suite /usr/local/bin/discus_suite_noparallel
+  else
+    sudo cp /usr/local/bin/discus_suite /usr/local/bin/discus_suite_noparallel
+  fi
+#
+fi
+#
+#   Always do a parallel version
+#
+export DIFFEV_MPI_FLAG=ON
+source   ./compile_discus.sh noclean
+#
+# Update version to the one actually compiled
+#
+export DISCUS_VERSION=$(cat ${DISCUS_INST_DIR}/develop/DiffuseBuild/lib_f90/version.inc)
+export DIFFUSE_PRE="DIFFUSE_${OPERATING_TYPE}_${OPERATING_NAME}_${OPERATING_VERSION}_v.${DISCUS_VERSION}"
+#
+cd $DISCUS_INST_DIR
+#
+if [[ "$OPERATING" == "DISCUS_WSL_LINUX" ]]; then          # WINDOWS WSL
+#
+# We need the shellscripts to start discus_suite
+#
+  if [[ $DISCUS_INSTALL == $DISCUS_LOCAL ]]; then
+    cp SHELLS/discus_suite_ubuntu.sh     /usr/local/bin
+    cp SHELLS/discus_suite_run_ubuntu.sh /usr/local/bin
+    cp SHELLS/terminal_wrapper.sh        /usr/local/bin
+  else
+    sudo cp SHELLS/discus_suite_ubuntu.sh     /usr/local/bin
+    sudo cp SHELLS/discus_suite_run_ubuntu.sh /usr/local/bin
+    sudo cp SHELLS/terminal_wrapper.sh        /usr/local/bin
+  fi
+fi
+export DISCUS_WAS_COMPILED="YES"
