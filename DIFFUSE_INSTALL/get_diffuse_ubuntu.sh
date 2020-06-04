f67fec8aae65241777ac66bcc203015f2d807d5a
rneder <reinhard.neder@fau.de>
2020-06-04 23:10:56 +0200

    Replaced "$1" by value of DISCUS_TAR_SOURCE

--- DIFFUSE_INSTALL/get_diffuse_ubuntu.sh
+++ DIFFUSE_INSTALL/get_diffuse_ubuntu.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #
 if [[ ! ${DISCUS_DO_COMPILE} == "COMPILE" ]]; then
-if [ ! -e DIFFUSE_${OPERATING_NAME}_${OPERATING_VERSION}.tar.gz ]; then
+if [ ! -e ${DIFFUSE_PRE}.tar.gz ]; then
 #
 #  File does not exist, obtain
 #
   export DISCUS_SUP_DOWN='https://github.com/rneder/DiffuseSuplement/releases/download/'
   export DISCUS_SUPPLEMENT=$(curl --silent "https://github.com/rneder/DiffuseSuplement/releases/latest" | grep -Poe 'v.[0-9]*.[0-9]*.[0-9]*')
-  export DISCUS_UBUNTU_URL=${DISCUS_SUP_DOWN}${DISCUS_SUPPLEMENT}'/DIFFUSE_'${OPERATING_NAME}'_'${OPERATING_VERSION}'.tar.gz'
+  export DISCUS_UBUNTU_URL=${DISCUS_SUP_DOWN}${DISCUS_SUPPLEMENT}'/'${DIFFUSE_PRE}'.tar.gz'
   if curl --output /dev/null --silent --head --fail "${DISCUS_UBUNTU_URL}"; then
     echo
     echo "Downloading ${DISCUS_UBUNTU_URL}"
     echo
-    curl -o DIFFUSE_${OPERATING_NAME}_${OPERATING_VERSION}.tar.gz -SL ${DISCUS_UBUNTU_URL}
+    curl -o ${DIFFUSE_PRE}.tar.gz -SL ${DISCUS_UBUNTU_URL}
     echo
   else
-    echo ${DISCUS_UBUNTU_URL} does not exist exists
+    echo ${DISCUS_UBUNTU_URL} does not exist
   fi
 fi
 fi
