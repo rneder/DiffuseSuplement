3c00f91328019cb5a0e38878c9b4b5e4012860d6
rneder <reinhard.neder@fau.de>
2020-06-04 22:44:45 +0200

    Changed mode to u+x

--- DIFFUSE_INSTALL/build_distribution.sh
+++ DIFFUSE_INSTALL/build_distribution.sh
@@ -4,36 +4,43 @@
 #
 cd ${DISCUS_INST_DIR}
 #
-export DISCUS_COMPILED_VERSION=$(cat ${DISCUS_INST_DIR}/develop/DiffuseBuild/lib_f90/version.inc)
-export DISCUS_DIST="DIFFUSE_"${OPERATING_NAME}_${OPERATING_VERSION}_${DISCUS_COMPILED_VERSION}
-mkdir -p ${DISCUS_DIST}/bin
-mkdir -p ${DISCUS_DIST}/share
-mkdir -p ${DISCUS_DIST}/pgplot
+mkdir -p ${DIFFUSE_PRE}/bin
+mkdir -p ${DIFFUSE_PRE}/share
+mkdir -p ${DIFFUSE_PRE}/pgplot
 #
-cp -r ${DISCUS_BIN_PREFIX}/pgplot/pgxwin_server     ${DISCUS_DIST}/pgplot  > /dev/null || :
-cp -r ${DISCUS_BIN_PREFIX}/pgplot/rgb.txt           ${DISCUS_DIST}/pgplot  > /dev/null || :
-cp -r ${DISCUS_BIN_PREFIX}/pgplot/grfont.dat        ${DISCUS_DIST}/pgplot  > /dev/null || :
-cp -r ${DISCUS_BIN_PREFIX}/pgplot/libpgplot.a       ${DISCUS_DIST}/pgplot  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/pgplot/pgxwin_server           ${DIFFUSE_PRE}/pgplot  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/pgplot/rgb.txt                 ${DIFFUSE_PRE}/pgplot  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/pgplot/grfont.dat              ${DIFFUSE_PRE}/pgplot  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/pgplot/libpgplot.a             ${DIFFUSE_PRE}/pgplot  > /dev/null || :
 #
-cp    ${DISCUS_BIN_PREFIX}/bin/discus_suite*        ${DISCUS_DIST}/bin  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/bin/terminal_wrapper.sh  ${DISCUS_DIST}/bin  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/pgplot/pgxwin_server     ${DISCUS_DIST}/bin  > /dev/null || :
+if [[ "$OPERATING" == "DISCUS_LINUX" ]]; then                # Native Linux
+  cp  ${DISCUS_BIN_PREFIX}/bin/discus_suite               ${DIFFUSE_PRE}/bin  > /dev/null || :
+elif [[ "$OPERATING" == "DISCUS_WSL_LINUX" ]]; then          # WINDOWS WSL
+  cp  ${DISCUS_BIN_PREFIX}/bin/discus_suite               ${DIFFUSE_PRE}/bin  > /dev/null || :
+  cp  ${DISCUS_BIN_PREFIX}/bin/discus_suite_noparallel    ${DIFFUSE_PRE}/bin  > /dev/null || :
+  cp  ${DISCUS_BIN_PREFIX}/bin/discus_suite_run_ubuntu.sh ${DIFFUSE_PRE}/bin  > /dev/null || :
+  cp  ${DISCUS_BIN_PREFIX}/bin/discus_suite_ubuntu.sh     ${DIFFUSE_PRE}/bin  > /dev/null || :
+  cp  ${DISCUS_BIN_PREFIX}/bin/terminal_wrapper.sh        ${DIFFUSE_PRE}/bin  > /dev/null || :
+  cp  ${DISCUS_BIN_PREFIX}/pgplot/pgxwin_server           ${DIFFUSE_PRE}/bin  > /dev/null || :
+elif  [[ "$OPERATING" == "DISCUS_MACOS" ]]; then              # MAC OS
+  cp  ${DISCUS_BIN_PREFIX}/bin/discus_suite               ${DIFFUSE_PRE}/bin  > /dev/null || :
+fi
 #
-cp    ${DISCUS_BIN_PREFIX}/share/AAA_INSTALL*.pdf   ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/discus_man.pdf     ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/diffev_man.pdf     ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/kuplot_man.pdf     ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/refine_man.pdf     ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/suite_man.pdf      ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/package_man.pdf    ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/color.map          ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/DiscusSuite.txt    ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/suite.hlp          ${DISCUS_DIST}/share  > /dev/null || :
-cp    ${DISCUS_BIN_PREFIX}/share/grfont.dat         ${DISCUS_DIST}/share  > /dev/null || :
-cp -r ${DISCUS_BIN_PREFIX}/share/discus/            ${DISCUS_DIST}/share  > /dev/null || :
-cp -r ${DISCUS_BIN_PREFIX}/share/diffev/            ${DISCUS_DIST}/share  > /dev/null || :
-cp -r ${DISCUS_BIN_PREFIX}/share/kuplot/            ${DISCUS_DIST}/share  > /dev/null || :
-cp -r ${DISCUS_BIN_PREFIX}/share/refine/            ${DISCUS_DIST}/share  > /dev/null || :
-cp -r ${DISCUS_BIN_PREFIX}/share/suite/             ${DISCUS_DIST}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/AAA_INSTALL*.pdf         ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/discus_man.pdf           ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/diffev_man.pdf           ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/kuplot_man.pdf           ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/refine_man.pdf           ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/suite_man.pdf            ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/package_man.pdf          ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/color.map                ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/DiscusSuite.txt          ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/suite.hlp                ${DIFFUSE_PRE}/share  > /dev/null || :
+cp    ${DISCUS_BIN_PREFIX}/share/grfont.dat               ${DIFFUSE_PRE}/share  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/share/discus/                  ${DIFFUSE_PRE}/share  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/share/diffev/                  ${DIFFUSE_PRE}/share  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/share/kuplot/                  ${DIFFUSE_PRE}/share  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/share/refine/                  ${DIFFUSE_PRE}/share  > /dev/null || :
+cp -r ${DISCUS_BIN_PREFIX}/share/suite/                   ${DIFFUSE_PRE}/share  > /dev/null || :
 #
-tar -zcf ${DISCUS_DIST}.tar.gz ${DISCUS_DIST}
+tar -zcf ${DIFFUSE_PRE}.tar.gz ${DIFFUSE_PRE}
