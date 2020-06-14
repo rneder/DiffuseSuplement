e69ffe1ea846189cf99640bdc27b243782f1bdd3
rneder <reinhard.neder@fau.de>
2020-06-14 12:26:54 +0200

    Added script to get precompiled MAC version

---
+++ DIFFUSE_INSTALL/get_diffuse_mac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+if [[ ! ${DISCUS_DO_COMPILE} == "COMPILE" ]]; then
+if [ ! -e ${DIFFUSE_PRE}.tar.gz ]; then
+#
+#  File does not exist, obtain
+#
+  export DISCUS_SUP_DOWN='https://github.com/rneder/DiffuseSuplement/releases/download/'
+  export DISCUS_SUPPLEMENT=$(curl --silent "https://github.com/rneder/DiffuseSuplement/releases/latest" | grep -oe 'v.[0-9]*.[0-9]*.[0-9]*')
+  export DISCUS_MAC_URL=${DISCUS_SUP_DOWN}${DISCUS_SUPPLEMENT}'/'${DIFFUSE_PRE}'.tar.gz'
+  if curl --output /dev/null --silent --head --fail "${DISCUS_MAC_URL}"; then
+    echo
+    echo "Downloading ${DISCUS_MAC_URL}"
+    echo
+    curl -o ${DIFFUSE_PRE}.tar.gz -SL ${DISCUS_MAC_URL}
+    echo
+  else
+    echo ${DISCUS_MAC_URL} does not exist
+  fi
+fi
+fi
