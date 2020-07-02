0ee4100ee898d9d734e5b1bc315cf4aa2cb49973
rneder <reinhard.neder@fau.de>
2020-07-02 17:58:11 +0200

    added ID_LIKE, removed blanks from OPERATING_NAME

--- DIFFUSE_INSTALL/find_user.sh
+++ DIFFUSE_INSTALL/find_user.sh
@@ -19,14 +19,16 @@ if [[ "$OSTYPE" == *"linux"* ]]; then
     export DISCUS_USER=$USER
     export WINDOWS_USER=$(env | grep "PATH=" | sed -nr '0,/.*Users\/(\w+).*/ s//\1/p' )
   fi
-  export OPERATING_NAME=$(cat /etc/os-release | grep -v PRETTY | grep 'NAME="' | sed 's:NAME="::' | sed 's:"::' | sed s': ::g' )
+  export OPERATING_NAME=$(cat /etc/os-release | grep -v PRETTY | grep 'NAME="' | sed 's:NAME="::' | sed 's:"::' | sed 's: ::g')
   export OPERATING_VERSION=$(lsb_release -r | sed "s.Release:.." | sed "s:\.::" | sed "s:\t::" )
+  export OPERATING_ID_LIKE=$(cat /etc/os-release | grep 'ID_LIKE=' | sed 's:ID_LIKE=::')
 elif [[ "$OSTYPE" == "cygwin" ]]; then
   export OPERATING=DISCUS_CYGWIN
   export OPERATING_TYPE="CYGWIN"
   export DISCUS_USER=$USER
   export WINDOWS_USER=$(env | grep "PATH=" | sed -nr '0,/.*Users\/(\w+).*/ s//\1/p' )  
   export OPERATING_VERSION="CYGWIN"
+  export OPERATING_ID_LIKE=$(cat /etc/os-release | grep 'ID_LIKE=' | sed 's:ID_LIKE=::')
 elif [[ "$OSTYPE" == *"darwin"* ]]; then
   export OPERATING=DISCUS_MACOS
   export OPERATING_TYPE="MAC"
@@ -34,12 +36,14 @@ elif [[ "$OSTYPE" == *"darwin"* ]]; then
   export OPERATING_VERSION=$(sw_vers -productVersion | sed 's,ProductVersion:,,' | sed 's, ,,' | sed "s:\.::" | sed "s:\..*::")
   export DISCUS_USER=$USER
   export WINDOWS_USER=""
+  export OPERATING_ID_LIKE="darwin"
 fi
 export MY_SHELL=$(echo $SHELL | sed 's/\/bin\///')
 export MY_SHELL_RC=$(echo ${MY_SHELL} | sed "s:${MY_SHELL}:.${MY_SHELL}rc:")
 echo 'Operating type: ' $OPERATING_TYPE
 echo 'Operating is  : ' $OPERATING
 echo 'Operating Name: ' $OPERATING_NAME
+echo 'Operating Like: ' $OPERATING_ID_LIKE
 echo 'Version   is  : ' $OPERATING_VERSION
 echo 'Shell     is  : ' $SHELL
 echo 'My_Shell  is  : ' $MY_SHELL
