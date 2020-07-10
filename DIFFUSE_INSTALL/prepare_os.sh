0d824c54d512920fbe2a0337f457dd025bc463a0
rneder <reinhard.neder@fau.de>
2020-07-10 08:46:53 +0200

    Added a check for Fortran compiler

--- DIFFUSE_INSTALL/prepare_os.sh
+++ DIFFUSE_INSTALL/prepare_os.sh
@@ -3,15 +3,14 @@
 # prepares the operating system
 #
 #  Currently avaliable for UBUNTU, CYGWIN, MACOS
-echo $OPERATING
 #
 if [[ "$OPERATING" == "DISCUS_LINUX" ]]; then
 #
-  if [[ "$OPERATING_ID_LIKE" == "arch" ]]; then
+  if [[ "$OPERATING_ID_LIKE" == "arch" ]]; then      # Arch-Liunx; Manjaro
     source prepare_arch.sh
-  elif [[ "$OPERATING_ID_LIKE" == "debian" ]]; then
+  elif [[ "$OPERATING_ID_LIKE" == "debian" ]]; then  # Debian; Ubuntu
     source prepare_debian.sh
-  elif [[ "$OPERATING_ID_LIKE" == "fedora" ]]; then
+  elif [[ "$OPERATING_ID_LIKE" == "fedora" ]]; then  # Redhat; Fedora; CentOs (needs work in find_user.sh)
     source prepare_fedora.sh
   else
     source prepare_linux.sh
