26be53734fc25448e9c911ac75e14085431280cf
rneder <reinhard.neder@fau.de>
2023-06-08 16:20:07 +0200

    Changed mode to executable

--- DIFFUSE_INSTALL/prepare_suite_bat.sh
+++ DIFFUSE_INSTALL/prepare_suite_bat.sh
@@ -4,7 +4,7 @@ echo 'REM  BAT does not have an "elseif"'                   >> DiscusWSL/discus_
 #
 echo 'if exist "C:\Program Files\Powershell\8" ('           >> DiscusWSL/discus_suite_ps1.bat
 echo '  REM  echo PWSH Version 8; sometime in the future ?' >> DiscusWSL/discus_suite_ps1.bat
-WSL_BAT='"C:\Program Files\Powershell\8\pwsh.exe" -File "'
+WSL_BAT='"C:\Program Files\Powershell\8\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
 WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_suite.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_suite_ps1.bat
@@ -12,8 +12,8 @@ echo '  exit'                                               >> DiscusWSL/discus_
 echo ')'                                                    >> DiscusWSL/discus_suite_ps1.bat
 #
 echo 'if exist "C:\Program Files\Powershell\7" ('           >> DiscusWSL/discus_suite_ps1.bat
-echo '  REM  echo PWSH Version 7; sometime in the future ?' >> DiscusWSL/discus_suite_ps1.bat
-WSL_BAT='"C:\Program Files\Powershell\7\pwsh.exe" -File "'
+echo '  REM  echo PWSH Version 7; current powershell      ' >> DiscusWSL/discus_suite_ps1.bat
+WSL_BAT='"C:\Program Files\Powershell\7\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
 WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_suite.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_suite_ps1.bat
@@ -21,18 +21,18 @@ echo '  exit'                                               >> DiscusWSL/discus_
 echo ')'                                                    >> DiscusWSL/discus_suite_ps1.bat
 #
 echo 'if exist "C:\Program Files\Powershell\6" ('           >> DiscusWSL/discus_suite_ps1.bat
-echo '  REM  echo PWSH Version 6; sometime in the future ?' >> DiscusWSL/discus_suite_ps1.bat
-WSL_BAT='"C:\Program Files\Powershell\6\pwsh.exe" -File "'
+echo '  REM  echo PWSH Version 6; may exist on older     ?' >> DiscusWSL/discus_suite_ps1.bat
+WSL_BAT='"C:\Program Files\Powershell\6\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
 WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_suite.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_suite_ps1.bat
 echo '  exit'                                               >> DiscusWSL/discus_suite_ps1.bat
 echo ')'                                                    >> DiscusWSL/discus_suite_ps1.bat
 echo 'REM Default if no pwsh.exe is detected'               >> DiscusWSL/discus_suite_ps1.bat
-WSL_BAT='C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File "'
+WSL_BAT='C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
 WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_suite.ps1"'
 echo ${WSL_BAT}                                             >> DiscusWSL/discus_suite_ps1.bat
 #
 echo
-cat DiscusWSL/discus_suite_ps1.bat
+#cat DiscusWSL/discus_suite_ps1.bat
--- DIFFUSE_INSTALL/prepare_terminal_bat.sh
+++ DIFFUSE_INSTALL/prepare_terminal_bat.sh
@@ -1,11 +1,11 @@
-WSL_USER_PROFILE='C:\Users\reinhard'
+#WSL_USER_PROFILE='C:\Users\reinhard'
 echo 'REM  Search for latest powershell; run with explicit path and exit' > DiscusWSL/discus_terminal_ps1.bat
 echo 'REM  Some DISCUS users do not have the PATH set up all that well'  >> DiscusWSL/discus_terminal_ps1.bat
 echo 'REM  BAT does not have an "elseif"'                   >> DiscusWSL/discus_terminal_ps1.bat
 #
 echo 'if exist "C:\Program Files\Powershell\8" ('           >> DiscusWSL/discus_terminal_ps1.bat
 echo '  REM  echo PWSH Version 8; sometime in the future ?' >> DiscusWSL/discus_terminal_ps1.bat
-WSL_BAT='"C:\Program Files\Powershell\8\pwsh.exe" -File "'
+WSL_BAT='"C:\Program Files\Powershell\8\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
 WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_terminal.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_terminal_ps1.bat
@@ -14,7 +14,7 @@ echo ')'                                                    >> DiscusWSL/discus_
 #
 echo 'if exist "C:\Program Files\Powershell\7" ('           >> DiscusWSL/discus_terminal_ps1.bat
 echo '  REM  echo PWSH Version 7'                           >> DiscusWSL/discus_terminal_ps1.bat
-WSL_BAT='"C:\Program Files\Powershell\7\pwsh.exe" -File "'
+WSL_BAT='"C:\Program Files\Powershell\7\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
 WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_terminal.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_terminal_ps1.bat
@@ -23,17 +23,17 @@ echo ')'                                                    >> DiscusWSL/discus_
 #
 echo 'if exist "C:\Program Files\Powershell\6" ('           >> DiscusWSL/discus_terminal_ps1.bat
 echo '  REM  echo PWSH Version 6'                           >> DiscusWSL/discus_terminal_ps1.bat
-WSL_BAT='"C:\Program Files\Powershell\6\pwsh.exe" -File "'
+WSL_BAT='"C:\Program Files\Powershell\6\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
 WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_terminal.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_terminal_ps1.bat
 echo '  exit'                                               >> DiscusWSL/discus_terminal_ps1.bat
 echo ')'                                                    >> DiscusWSL/discus_terminal_ps1.bat
 echo 'REM Default if no pwsh.exe is detected'               >> DiscusWSL/discus_terminal_ps1.bat
-WSL_BAT='C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File "'
+WSL_BAT='C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
 WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_terminal.ps1"'
 echo ${WSL_BAT}                                             >> DiscusWSL/discus_terminal_ps1.bat
 #
 echo
-cat DiscusWSL/discus_terminal_ps1.bat
+#cat DiscusWSL/discus_terminal_ps1.bat
