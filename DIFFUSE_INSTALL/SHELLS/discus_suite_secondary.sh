001713cdac1054c5e24200f584194a99953a237a
rneder <reinhard.neder@fau.de>
2023-06-12 15:37:23 +0200

    Removed obsolete scripts

--- DIFFUSE_INSTALL/SHELLS/discus_suite_secondary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This shell is called from /suite.sh if it is the second instance of discus_suite.
-# It calls /bin/discus_suite_run.sh,  which contains an error check upon exit.
-# /bin/xrdb  allows to merge X-terminal setting with /share/.Xresources  
-
-/bin/xrdb -merge /share/.Xresources
-/bin/xterm -fa "DejaVu Sans Mono" -fs 12 -rightbar -sb -pob -title "Discus_Suite secondary window exit first" -e /bin/discus_suite_run.sh
--- DIFFUSE_INSTALL/SHELLS/kuplot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if( /bin/ps | /bin/grep XWin ); then
-   export DISPLAY=':0';
-   /bin/xterm -rightbar -sb -pob -title "kuplot secondary window" -e /bin/kuplot.exe;
-else
-   /bin/rm -f /tmp/.X*-lock
-   /bin/xinit "/bin/xterm" -rightbar -sb -pob -title "kuplot primary window exit last" -e /bin/kuplot.exe -- "/usr/bin/XWin" :0 -multiwindow -logfile /dev/null;
-fi
-
