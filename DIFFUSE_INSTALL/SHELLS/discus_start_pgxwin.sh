26975889a18093cf76abe3103235f7d0ae6c28e2
rneder <reinhard.neder@fau.de>
2023-06-12 15:47:06 +0200

    Added startup scripts

--- DIFFUSE_INSTALL/SHELLS/discus_start_pgxwin.sh
+++ DIFFUSE_INSTALL/SHELLS/discus_start_pgxwin.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-export DISPLAY=:0
-pgxwin_server &
+if [[ "$1" == "1" ]]; then
+# FIRST is WSL 1
+  export DISPLAY=:0
+else
+# Second is WSL 2
+  if [[ "$2" == "11" ]]; then
+    export DISPLAY=:0
+  else
+    export DISPLAY=$(awk '/nameserver / { print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+  fi
+fi
+terminator --execute discus_suite -macro $HOME/.DISCUS/discus_auto.mac 
--- DIFFUSE_INSTALL/SHELLS/discus_startup.ps1
+++ DIFFUSE_INSTALL/SHELLS/discus_startup.ps1
@@ -4,21 +4,21 @@
 cd "$HOME"
 cd DISCUS_INSTALLATION
 $dis_inst = get-Location | Select-Object Path -ExpandProperty Path
-cd DISCUS_INSTALLATION\DiscusWSL
+cd DiscusWSL
 $cwd = get-Location | Select-Object Path -ExpandProperty Path
 #
-$wsl_use = & $dis_inst\get_wsl_ver.ps1
-$win_ver = & $dis_inst\get_win_ver.ps1
-$UBUNTU_EXE = & $dis_inst\get_ubuntu.ps1
+$wsl_use = & "$dis_inst\get_wsl_ver.ps1"
+$win_ver = & "$dis_inst\get_win_ver.ps1"
+$UBUNTU_EXE = & "$dis_inst\get_ubuntu.ps1"
 #
 if( -not ($wsl_use -eq "2" -And $win_ver -eq "11")) {
-  & .\xlaunch.ps1
+  & "$dis_inst\xlaunch.ps1"
 }
 #
-cd "$HOME"
+#cd "$HOME"
 #
-Start-Process "$UBUNTU_EXE" -WindowStyle Hidden -ArgumentList "-c `"cd ; source .profile.local; export DISPLAY=:0; $HOME/DIFFUSE_INSTALLATION/SHELLS/start_pgxwin.sh; exit`""
+Start-Process "$UBUNTU_EXE" -WindowStyle Hidden -ArgumentList "-c `"cd ; source .profile.local; source /usr/local/bin/discus_start_pgxwin.sh $wsl_use $win_ver `""
 
-$done = (Read-Host 'Type enter to finish POWERSHELL')
-exit
+#$done = (Read-Host 'Type enter to finish POWERSHELL')
+#exit
 #
