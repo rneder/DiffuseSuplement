80f4f132a222a648c73377a2bd745d6d5911c4f0
rneder <reinhard.neder@fau.de>
2023-06-12 15:54:03 +0200

    Corrected script to discus_startup

--- DIFFUSE_INSTALL/prepare_startup_bat.sh
+++ DIFFUSE_INSTALL/prepare_startup_bat.sh
@@ -6,7 +6,7 @@ echo 'if exist "C:\Program Files\Powershell\8" ('           >> DiscusWSL/discus_
 echo '  REM  echo PWSH Version 8; sometime in the future ?' >> DiscusWSL/discus_startup.bat
 WSL_BAT='"C:\Program Files\Powershell\8\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
-WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_suite.ps1"'
+WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_startup.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_startup.bat
 echo '  exit'                                               >> DiscusWSL/discus_startup.bat
 echo ')'                                                    >> DiscusWSL/discus_startup.bat
@@ -15,7 +15,7 @@ echo 'if exist "C:\Program Files\Powershell\7" ('           >> DiscusWSL/discus_
 echo '  REM  echo PWSH Version 7; current powershell      ' >> DiscusWSL/discus_startup.bat
 WSL_BAT='"C:\Program Files\Powershell\7\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
-WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_suite.ps1"'
+WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_startup.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_startup.bat
 echo '  exit'                                               >> DiscusWSL/discus_startup.bat
 echo ')'                                                    >> DiscusWSL/discus_startup.bat
@@ -24,14 +24,14 @@ echo 'if exist "C:\Program Files\Powershell\6" ('           >> DiscusWSL/discus_
 echo '  REM  echo PWSH Version 6; may exist on older     ?' >> DiscusWSL/discus_startup.bat
 WSL_BAT='"C:\Program Files\Powershell\6\pwsh.exe" -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
-WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_suite.ps1"'
+WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_startup.ps1"'
 echo '  '${WSL_BAT}                                         >> DiscusWSL/discus_startup.bat
 echo '  exit'                                               >> DiscusWSL/discus_startup.bat
 echo ')'                                                    >> DiscusWSL/discus_startup.bat
 echo 'REM Default if no pwsh.exe is detected'               >> DiscusWSL/discus_startup.bat
 WSL_BAT='C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File "'
 WSL_BAT+=${WSL_USER_PROFILE}
-WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_suite.ps1"'
+WSL_BAT+='\DISCUS_INSTALLATION\DiscusWSL\discus_startup.ps1"'
 echo ${WSL_BAT}                                             >> DiscusWSL/discus_startup.bat
 #
 echo
